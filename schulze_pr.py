ec866422b6314ea51f83882c699ef69d65b931e6
Brad Beattie <bradbeattie@gmail.com>
2010-11-10 18:57:37 -0800

    Fixing documentation

--- schulze_pr.py
+++ schulze_pr.py
@@ -13,7 +13,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-# This class implements Schulze STV, a proportional representation system
+# This class implements the Schulze Proportional Ranking Method as defined
+# in schulze2.pdf
 from schulze_stv import SchulzeSTV
 from pygraph.classes.digraph import digraph
 
