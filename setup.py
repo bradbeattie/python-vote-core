3f9405e2d8e3522ec53b0294c202c5403f433d38
Brad Beattie <bradbeattie@gmail.com>
2012-04-23 19:59:03 -0700

    Updating version number

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ requires = [
     ]
 
 setup(name='python-vote-core',
-      version='20110509.1',
+      version='20120423.0',
       description="An implementation of various election methods, most notably the Schulze Method and Schulze STV.",
       long_description=README + '\n\n' + CHANGES + '\n\n' + LICENSE,
       classifiers=[
